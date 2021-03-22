81920a79928cbcf582070372fdafd7306400f74e
Hln20 <77648292+Hln20@users.noreply.github.com>
2021-03-22 14:53:55 +0300

    Delete src/ru/geekbrains/homework_2 directory

--- src/ru/geekbrains/homework_2/Lesson2/MyArrayDataException.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.homework_2.Lesson2;
-
-public class MyArrayDataException extends Exception{
-    public MyArrayDataException(String message) {
-        super("Wrong data " + message);
-    }
-}
-
--- src/ru/geekbrains/homework_2/Lesson2/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.homework_2.Lesson2;
-
-public class MyArraySizeException extends RuntimeException{
-    public MyArraySizeException() {
-        super("Wrong size");
-    }
-}
